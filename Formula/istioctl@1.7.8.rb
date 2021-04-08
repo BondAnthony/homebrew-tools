5e40f65b07648cc8a41c84e418cf5eef6b2a267d
AJ Bond <aj@influxdata.com>
2021-04-08 11:49:44 -0400

    Istio 1.7.8

---
+++ Formula/istioctl@1.7.8.rb
@@ -0,0 +1,14 @@
+class IstioctlAT168 < Formula
+  desc "Connect, secure, control, and observe services."
+  homepage "https://istio.io"
+  url "https://github.com/istio/istio/releases/download/1.7.8/istioctl-1.7.8-osx.tar.gz"
+  sha256 "0f06bae72e828da836cd6cbe2e197730bc9c31c1606a259f8c36b4ffc561ff99"
+  license "Apache-2.0"
+  def install
+    bin.install "istioctl"
+  end
+  
+  test do
+    system "#{bin}/istioctl version"
+  end
+end
