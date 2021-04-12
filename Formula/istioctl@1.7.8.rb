5ba2a8636914cd121c44e0e67f9afbd7d215d54b
AJ Bond <aj@influxdata.com>
2021-04-12 10:17:15 -0400

    Istioctl 1.7.8 sha

--- Formula/istioctl@1.7.8.rb
+++ Formula/istioctl@1.7.8.rb
@@ -2,7 +2,7 @@ class IstioctlAT178 < Formula
   desc "Connect, secure, control, and observe services."
   homepage "https://istio.io"
   url "https://github.com/istio/istio/releases/download/1.7.8/istioctl-1.7.8-osx.tar.gz"
-  sha256 "0f06bae72e828da836cd6cbe2e197730bc9c31c1606a259f8c36b4ffc561ff99"
+  sha256 "55c0e169ea98519f1e45b5c9302ef61913f95b33bf46a7d98ce4b07cad54bc6b"
   license "Apache-2.0"
   def install
     bin.install "istioctl"
