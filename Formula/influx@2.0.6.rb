72dcdf7c7b86e02c361278c943023dec6d5d26b1
AJ Bond <aj@influxdata.com>
2021-05-10 12:50:22 -0400

    fix(influx): upgrade influx url

--- Formula/influx@2.0.6.rb
+++ Formula/influx@2.0.6.rb
@@ -1,7 +1,7 @@
 class InfluxAT206 < Formula
   desc "InfluxData CLI for InfluxDB 2.0.6"
   homepage "https://influxdata.com"
-  url "https://dl.influxdata.com/influxdb/releases/influxdb2_client_2.0.6_darwin_amd64.tar.gz"
+  url "https://dl.influxdata.com/influxdb/releases/influxdb2-client-2.0.6-darwin-amd64.tar.gz"
   sha256 "db62fd5be1fbe690a01b7f3aaf1f4c44ceabd1a8e5d95365a2e33553ed868ac2"
   license "MIT"
   def install
